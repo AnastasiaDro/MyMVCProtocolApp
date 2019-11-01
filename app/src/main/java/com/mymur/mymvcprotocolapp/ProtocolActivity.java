08cedbbc8f622d423ba7da37b2f03eb2679e1446
AnastasiaDro <adrogunova@gmail.com>
2019-11-01 12:16:25 +0300

    -found Btns by ID in ProtocolActivity
    TODO:
    
    - make selection of trial in RecyclerView: add actions to buttons of trialAnswer

--- app/src/main/java/com/mymur/mymvcprotocolapp/ProtocolActivity.java
+++ app/src/main/java/com/mymur/mymvcprotocolapp/ProtocolActivity.java
@@ -3,6 +3,7 @@ package com.mymur.mymvcprotocolapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ProtocolActivity extends AppCompatActivity {
 
@@ -11,7 +12,13 @@ public class ProtocolActivity extends AppCompatActivity {
     DataBaseHelper dbHelper;
     int placeId = R.id.placeholderForProtocol;
     private MyData myData;
+    int selectedStudentId;
+    int selectedTrialId;
+    int resultCode;
 
+    Button badBtn;
+    Button withHintBtn;
+    Button goodBtn;
 
 
     @Override
@@ -28,6 +35,11 @@ public class ProtocolActivity extends AppCompatActivity {
         ListFragment fragment = new ListFragment(activityName, myData, placeId);
         fragment.setArguments(bundle);
         fragment.postFragment(this);
+
+        badBtn = findViewById(R.id.btnBadAnswer);
+        withHintBtn = findViewById(R.id.btnWHanswer);
+        goodBtn= findViewById(R.id.btnGoodAnswer);
+
     }
 
 
